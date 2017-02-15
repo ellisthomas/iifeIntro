cfa09429c286dd4ec199a55ac974d74f0f6c9a39
ellisthomas <ellisthomas@gmail.com>
2017-02-14 19:48:38 -0600

    worked on some iife

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("hello");
+var outside = "global";
+
+function scopeStuff() {
+	var inside = "local";
+	// console.log("outside from inside the function", outside); 
+	// console.log("inside from inside the function", inside); 
+}
+
+// console.log("outside from outside the fucntion", outside);
+// // console.log("inside from outside the function", inside);
+
+// scopeStuff();
+
+
+var global_base = 500;
+(function throwAway(){
+	global_base+=500;
+	console.log(global_base);
+})();
+
+var cats = (function(){
+	var color = "black";
+	var type = "tiger";
+	
+	return {
+		getColor : function(){
+			return color;
+		},
+		getType : function(){
+			return type;
+		},
+		setColor : function(newColor){
+			color = newColor;
+		}
+	};
+})();
+
+console.log(cats.getColor()); 
+
+
+// throwAway();
+
+
