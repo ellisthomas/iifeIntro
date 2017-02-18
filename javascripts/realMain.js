c1e2f4c1874441591681bc2be61a7286b4fd865f
ellisthomas <ellisthomas@gmail.com>
2017-02-18 11:20:18 -0600

    all pages laid out

---
+++ javascripts/realMain.js
@@ -0,0 +1,9 @@
+console.log("Planets: ", SolarSystem.getPlanets());
+
+console.log("Planets with people: ", SolarSystem.getPlanetsLanedPeopleOn);
+
+SolarSystem.setSpaceships("Voyager 1");
+SolarSystem.setSpaceships("Voyager 2");
+SolarSystem.setSpaceships("Gemini");
+SolarSystem.setSpaceships("Apollo");
+console.log("Spaceships: ", SolarSystem.getSpaceships());
